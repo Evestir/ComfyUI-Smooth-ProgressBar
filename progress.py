de1823c39cf68c5c731451804697d089d68321b9
Evestir <54894315+Evestir@users.noreply.github.com>
2024-09-08 18:23:05 +0900

    Add files via upload

---
+++ progress.py
@@ -0,0 +1,74 @@
+from time import sleep
+import threading
+
+#-------------- Easing Functions --------------#
+def BezierBlend(t: float):
+    sqr = t * t
+    return sqr / (2.0 * (sqr - t) + 1.0)
+
+def easeOutCirc(t: float):
+    return math.sqrt(1 - math.pow(t - 1, 2))
+
+def easeInOutBack(t: float):    
+    c1 = 1.70158
+    c2 = 2.5949095
+    ret = 0
+
+    if t < 0.5:
+        ret = (math.pow(2 * t, 2) * ((c2 + 1) * 2 * t - c2)) / 2
+    else:
+        ret = (math.pow(2 * t - 2, 2) * ((c2 + 1) * (t * 2 - 2) + c2) + 2) / 2
+    return ret
+#----------------------------------------------#
+
+def is_odd(n):
+    if n % 2 == 0:
+        return False
+    else:
+        return True
+
+#--------------- Configuration ----------------#
+sleepTime = 0.0025
+steps = 100
+multiplyFactor = math.floor(10000 / steps) / 10000
+#----------------------------------------------#
+
+class ProgressBar:
+    def __init__(self, total):
+        global PROGRESS_BAR_HOOK
+        self.total = total
+        self.current = 0
+        self.hook = PROGRESS_BAR_HOOK
+
+    def update_absolute(self, value, total=None, preview=None):
+        def hook():
+            if self.hook is not None:
+                    self.hook(self.current, self.total, preview)
+        if total is not None:
+            self.total = total
+        if value > self.total:
+            value = self.total
+
+        # Display progress only if the completed steps are even.
+        if is_odd(value) and value is not total:
+            return
+        def animate():
+            origin = self.current
+            diff = value - origin
+            for i in range(steps):
+                self.current = origin + (easeInOutBack(i * multiplyFactor) * diff)
+                sleep(sleepTime)
+                hook()
+            self.current = value
+            if value == total: # Once the progress bar reaches 100%, then this motion will be played.
+                for i in range(steps):
+                    self.current = total - (BezierBlend(i * multiplyFactor) * total)
+                    hook()
+                    sleep(sleepTime + 0.005)
+        thread = threading.Thread(target=animate)
+        thread.start()
+        if value == total:
+            thread.join()
+
+    def update(self, value):
+        self.update_absolute(self.current + value)
